b9af1f6cfd8615bcb4c6f876c6eed9216646d44b
Ivan <ivan.lapochkin@gmail.com>
2024-09-28 16:32:06 +0300

    added py generators

---
+++ task(without tqdm).py
@@ -0,0 +1,20 @@
+x = int(input("write x of 10^x numbers u wnt to solve with 3n+1: "))
+def collatz_sequence(x):
+    num_seq = [x]
+    if x < 1:
+        return []
+    while x > 1:
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = 3 * x + 1
+        # Added line
+        num_seq.append(x)
+    return num_seq
+
+f = open("collatz_sequence.txt", 'w')
+
+for i in range(1, x ** 10):
+    f.write(str(collatz_sequence(i)) + '\n')
+
+f.close()
---
+++ task.py
@@ -0,0 +1,25 @@
+from tqdm import tqdm
+# requires tqdm (pip install tqdm)
+
+x = int(input("write x of 10^x numbers u wnt to solve with 3n+1: "))
+
+def collatz_sequence(x):
+    num_seq = [x]
+    if x < 1:
+        return []
+    while x > 1:
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = 3 * x + 1
+        # Added line
+        num_seq.append(x)
+    return num_seq
+
+
+f = open("collatz_sequence.txt", 'w')
+
+for i in tqdm(range(1, x ** 10)):
+    f.write(str(collatz_sequence(i)) + '\n')
+
+f.close()
